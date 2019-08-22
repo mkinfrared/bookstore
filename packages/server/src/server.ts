4e8345ecb6cf57b69d84ca5bbe3cf6cc47089db2
mkinfrared <mkinfrared@gmail.com>
2019-08-22 11:22:10 +0300

    feat(#0): Init

---
+++ packages/server/src/server.ts
@@ -0,0 +1,21 @@
+import { add } from "@bookstore/common";
+import { SERVER_PORT } from "@util/secrets";
+import { Server } from "http";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+
+import app from "./app";
+
+export let server: Server;
+
+(async () => {
+  try {
+    await createConnection();
+    add(24, 28);
+    server = app.listen(SERVER_PORT, () =>
+      console.log(`Server is running on port ${SERVER_PORT}`)
+    );
+  } catch (e) {
+    console.log("Could not connect to database");
+  }
+})();
