290017ffb746920cafd45d4d135c4fedc4e6d19a
mkinfrared <mkinfrared@gmail.com>
2019-08-26 13:40:37 +0300

    feat(#8): Confirmation email
    * Email is sent to confirm the registration

--- packages/server/src/resolvers/confirm/confirm.test.ts
+++ packages/server/src/resolvers/confirm/confirm.test.ts
@@ -1,6 +1,6 @@
 import { User } from "@db/entity/User";
 import createConfirmationLink from "@util/createConfirmationLink";
-import { client } from "@util/redis";
+import { redis } from "@util/redis";
 import { SERVER_HOST } from "@util/secrets";
 import request from "graphql-request";
 import { mockReq } from "sinon-express-mock";
@@ -9,6 +9,11 @@ describe("confirm resolver", () => {
   const email = "marklar@gmail.com";
   const username = "marklar";
   const password = "foobar";
+  const req = mockReq({
+    headers: {
+      host: "foobar"
+    }
+  });
   const mutation = (id: string) => `
   mutation{
     confirm(id: "${id}")
@@ -16,7 +21,7 @@ describe("confirm resolver", () => {
 `;
 
   beforeEach(() => {
-    client.flushall();
+    redis.flushall();
   });
 
   it("should return true when a correct registration ID is sent", async () => {
@@ -24,21 +29,23 @@ describe("confirm resolver", () => {
 
     await user.save();
 
-    const req = mockReq();
-    const link = await createConfirmationLink(user.id, client, req);
+    const link = await createConfirmationLink(user.id, redis, req);
     const regex = new RegExp("(?<=/confirm/).+", "gi");
     const ids = link.match(regex);
 
     expect(ids).toHaveLength(1);
 
     const id = ids![0];
-    const userID = await client.get(id);
+    const userID = await redis.get(id);
 
     expect(userID).toBe(user.id);
 
     const response = await request(SERVER_HOST, mutation(id));
 
+    await user.reload();
+
     expect(response.confirm).toBe(true);
+    expect(user.confirmed).toBe(true);
   });
 
   it("should return false when an incorrect registration ID is sent", async () => {
@@ -46,26 +53,31 @@ describe("confirm resolver", () => {
 
     await user.save();
 
-    const req = mockReq();
-    await createConfirmationLink(user.id, client, req);
+    await createConfirmationLink(user.id, redis, req);
 
     const id = "test";
-    let userID = await client.get(id);
+    let userID = await redis.get(id);
 
     expect(userID).toBe(null);
 
     const response = await request(SERVER_HOST, mutation(id));
 
+    await user.reload();
+
     expect(response.confirm).toBe(false);
+    expect(user.confirmed).toBe(false);
 
-    await client.set(id, "foobar");
+    await redis.set(id, "foobar");
 
-    userID = await client.get(id);
+    userID = await redis.get(id);
 
     expect(userID).toBe("foobar");
 
     const response2 = await request(SERVER_HOST, mutation(id));
 
+    await user.reload();
+
     expect(response2.confirm).toBe(false);
+    expect(user.confirmed).toBe(false);
   });
 });
